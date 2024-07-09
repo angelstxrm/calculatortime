131944533ae404b9c9b2b6a8ab5200f7faac0c2b
angelstxrm <danilbelenkin07@gmail.com>
2024-07-09 12:28:58 +0300

    added calculator views

--- calculator/views.py
+++ calculator/views.py
@@ -1,2 +1,27 @@
 from django.shortcuts import render
 from datetime import datetime, timedelta
+import pytz
+
+def time_calculator(request):
+    result = None
+    moscow_tz = pytz.timezone('Europe/Moscow')
+
+    if request.method == 'POST':
+        time1 = request.POST.get('time1')
+        time2 = request.POST.get('time2')
+        operation = request.POST.get('operation')
+
+        try:
+            time1 = datetime.strptime(time1, '%H:%M').replace(tzinfo=moscow_tz)
+            time2 = datetime.strptime(time2, '%H:%M').replace(tzinfo=moscow_tz)
+
+            if operation == 'add':
+                result_time = (time1 + timedelta(hours=time2.hour, minutes=time2.minute)).time()
+            elif operation == 'subtract':
+                result_time = (time1 - timedelta(hours=time2.hour, minutes=time2.minute)).time()
+
+            result = result_time.strftime('%H:%M')
+        except ValueError:
+            result = 'Invalid time format. Please use HH:MM.'
+        
+    return render(request, 'calculator/time_calculator.html', {'result': result})
--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from calculator.views import  time_calculator
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', time_calculator, name='time_calculator'),
 ]
