fbb960f1347823d0184792083930b546b9eae8bd
angelstxrm <danilbelenkin07@gmail.com>
2024-07-13 13:08:32 +0300

    upd: added page 404 notfound

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import render
+from django.conf.urls import handler404
 
 from calculator.views import  time_calculator
+
+def custom_404(request, exception):
+    return render(request, 'calculator/404.html', status=404)
+
+handler404 = custom_404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
